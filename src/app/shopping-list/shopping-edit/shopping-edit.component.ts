01d7ac636cf3ba7bd009ac69b5cf1d8dfa9dc711
m-tonon <math.tonon@gmail.com>
2023-01-14 05:45:08 -0300

    adding the shopping list service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, ViewChild, Output } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,13 +11,15 @@ export class ShoppingEditComponent {
   @ViewChild('nameInput', { static: false } ) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false } ) amountInputRef: ElementRef;
   // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  // @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor (private shoppingListService: ShoppingListService) {}
 
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredients(newIngredient);
   }
 
 }
