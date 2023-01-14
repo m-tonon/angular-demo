c3b7fc4c668ac89e1798dfd7efadc6114c126f20
m-tonon <math.tonon@gmail.com>
2023-01-14 06:01:44 -0300

    adding ingredients to recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
    // public - exposes the keys outside of the model
    // and that is how assign string in TypeScript property : type;
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -8,13 +9,20 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_860,ar_3:2/v1/img/recipes/30/32/45/tKqC3hipQA2MRyTEsneh_oven-bbq-ribs-02757.jpg'
-    ),
+      'https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_860,ar_3:2/v1/img/recipes/30/32/45/tKqC3hipQA2MRyTEsneh_oven-bbq-ribs-02757.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Potato', 5)
+      ]),
     new Recipe(
       'Another Test Recipe',
       'This is  simply a test',
-      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
-    )
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg',
+      [
+        new Ingredient('Onion', 1),
+        new Ingredient('Garlic clove', 4),
+        new Ingredient('Egg', 6)
+      ])
   ];
 
   getRecipes() {
