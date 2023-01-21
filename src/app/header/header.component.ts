0e2dc0c7b124acb0a04a6171f46db87248aef40a
m-tonon <math.tonon@gmail.com>
2023-01-21 06:08:21 -0300

    adding navigation and marking active routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class HeaderComponent {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature); // emit an event whenever the button is clicked
-  }
 }
