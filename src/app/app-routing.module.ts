38a61fd273d270d4b57403a88a24307879700221
m-tonon <math.tonon@gmail.com>
2023-01-21 06:53:22 -0300

    adding editing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,15 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent, children: [
     { path:'', component: RecipeStartComponent },
-    { path:':id', component: RecipeDetailComponent }
+    { path:'new', component: RecipeEditComponent },
+    { path:':id', component: RecipeDetailComponent },
+    { path:':id/edit', component: RecipeEditComponent },
   ]},
   { path: 'shopping-list', component: ShoppingListComponent},
 ]
