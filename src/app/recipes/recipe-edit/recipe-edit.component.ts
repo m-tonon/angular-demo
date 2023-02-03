58416bb8a0433583814715c29b2a3e3943dd9faf
m-tonon <math.tonon@gmail.com>
2023-02-03 06:12:57 -0300

    deleting all items in a FormArray

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -101,7 +101,8 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onDeleteIngredient(index: number) {
-    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    // (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    (<FormArray>this.recipeForm.get('ingredients')).clear();
   }
 
   onCancel() {
