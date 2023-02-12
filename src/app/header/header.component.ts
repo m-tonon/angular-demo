5292e5b1e7a06bd6bd7599331f4a97f810d8c551
m-tonon <math.tonon@gmail.com>
2023-02-12 18:23:49 -0300

    storing recipes into firebase database

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class HeaderComponent {
   collapsed = true;
+
+  constructor (private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,26 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    // get my recipe list from recipe service
+    const recipes = this.recipeService.getRecipes();
+    this.http
+    .put(
+      'https://ng-course-recipe-book-94c7b-default-rtdb.firebaseio.com/recipe.json',
+      recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+    }
+    // use 'put' to store more than one recipe and to overwrite any previous recipe stored
+    // this is an end point given by firebase.
 }
