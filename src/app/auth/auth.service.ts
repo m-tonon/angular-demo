10563d5d83b713b821c805eb209b8cd3e1b3e50b
m-tonon <math.tonon@gmail.com>
2023-02-14 07:32:00 -0300

    sending login request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean
 }
 
 @Injectable({
@@ -38,6 +39,17 @@ export class AuthService {
       }
       return throwError(errorMessage);
     }));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+    .post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDXTXaAbo6iyVEJ_deXyyUgcJ8GS4xzAHY',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    })
   }
 }
 
