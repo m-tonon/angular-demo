7b7e1ceff35d3635f74bccebe76b9ad4af6ac20b
m-tonon <math.tonon@gmail.com>
2023-02-16 06:33:47 -0300

    fixing token for login request

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -16,6 +16,9 @@ export class AuthInterceptorService implements HttpInterceptor {
     return this.authService.user.pipe(
       take(1),
       exhaustMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
         const modifiedReq = req.clone({
           params: new HttpParams().set('auth', user.token),
         });
