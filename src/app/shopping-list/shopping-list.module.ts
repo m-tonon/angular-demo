65a9cfe7ba8043af2c77a1fa38f8af863d3857b2
m-tonon <math.tonon@gmail.com>
2023-02-18 07:07:05 -0300

    splitting modules in shopping list component

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
+import { ShoppingListComponent } from "./shopping-list.component";
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ShoppingListRoutingModule
+  ]
+})
+
+export class ShoppingListModule {}
