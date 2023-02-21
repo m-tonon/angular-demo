324032db31166277335dcfb10996cbd4c8302a30
m-tonon <math.tonon@gmail.com>
2023-02-21 07:52:54 -0300

    adding actions

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,5 +1,7 @@
 import { Action } from "@ngrx/store";
-import { Ingredient } from "../shared/ingredient.model";
+
+import { Ingredient } from "../../shared/ingredient.model";
+import { ADD_INGREDIENT } from "./shopping-list.actions";
 
 const initialState = {
   ingredients: [
@@ -10,7 +12,7 @@ const initialState = {
 
 export function shoppingListReducer (state = initialState, action: Action) {
   switch (action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       return {
         ...state,
         // spread the content of the state into a new JS Object (a copy).
