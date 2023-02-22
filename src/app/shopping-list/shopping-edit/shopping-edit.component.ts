d1a55648800abb152d41a0af3415e021eb758449
m-tonon <math.tonon@gmail.com>
2023-02-22 06:48:54 -0300

    dispatching action to add ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import * as ShoppingListActions from '../store/shopping-list.actions'
 
 @Component({
   selector: 'app-shopping-edit',
@@ -18,7 +20,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
   editedItemIndex: number;
   editedItem: Ingredient;
 
-  constructor (private shoppingListService: ShoppingListService) {}
+  constructor (
+    private shoppingListService: ShoppingListService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    ) {}
 
   ngOnInit(): void {
     this.subscription = this.shoppingListService.startedEditing
@@ -41,7 +46,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
     if (this.editMode) {
       this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient)
     } else {
-      this.shoppingListService.addIngredients(newIngredient);
+      // this.shoppingListService.addIngredients(newIngredient);
+      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
     this.editMode = false;
     form.reset();
