650d60cb029c3f1a32ccd98de611d0e04b7679c6
m-tonon <math.tonon@gmail.com>
2023-02-27 06:57:47 -0300

    fixing error on login effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -19,35 +19,33 @@ export interface AuthResponseData {
 @Injectable()
 
 export class AuthEffects {
-  authLogin = createEffect(() => this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START),
-    switchMap((authData: AuthActions.LoginStart) => {
-      return this.http
-      .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-          environment.firebaseAPIKey,
-        {
-          email: authData.payload.email,
-          password: authData.payload.password,
-          returnSecureToken: true,
-        }
-      ).pipe(
-        map(resData => {
-          // ...
-          const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-          return of(
-            new AuthActions.Login({
-              email: resData.email,
-              userId: resData.localId,
-              token: resData.idToken,
-              expirationDate: expirationDate}));
-        }),
-        catchError(error => {
-        // ...
-        return of();
-      })
-      );
-    })
+  authLogin = createEffect(
+    () => this.actions$.pipe(ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart)=> {
+     return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+             environment.firebaseAPIKey,
+            {
+                email: authData.payload.email,
+                password: authData.payload.password,
+                returnSecureToken: true
+            }
+        ).pipe(
+          map(resData => {
+            const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+            return new AuthActions.Login({
+                email: resData.email,
+                userId: resData.localId,
+                token: resData.idToken,
+                expirationDate: expirationDate
+              });
+          }),
+          catchError(errorRes => {
+            return of();
+          })
+        );
+     })
+    )
   );
 
   constructor (
