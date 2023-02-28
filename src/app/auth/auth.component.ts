2245f914d0b3794d6536ae5b01f4565f5a81c8d1
m-tonon <math.tonon@gmail.com>
2023-02-28 06:39:51 -0300

    cleaning code

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -50,30 +50,15 @@ export class AuthComponent implements OnInit, OnDestroy {
     const email = form.value.email;
     const password = form.value.password;
 
-    let authObs: Observable<AuthResponseData>;
-
     this.isLoading = true;
 
     if (this.isLoginMode) {
-      // authObs = this.authService.login(email, password);
-      this.store.dispatch(new AuthActions.LoginStart({email: email, password: password}));
+      this.store.dispatch(
+        new AuthActions.LoginStart({email: email, password: password}));
     } else {
-      authObs = this.authService.signup(email, password);
+      this.store.dispatch(
+        new AuthActions.SignupStart({email: email, password: password}));
     }
-
-    // authObs.subscribe(
-    //   (resData) => {
-    //     console.log(resData);
-    //     this.isLoading = false;
-    //     this.router.navigate(['/recipes']);
-    //   },
-    //   (errorMessage) => {
-    //     console.log(errorMessage);
-    //     this.error = errorMessage;
-    //     this.showErrorAlert(errorMessage);
-    //     this.isLoading = false;
-    //   }
-    //);
 
     form.reset();
   }
