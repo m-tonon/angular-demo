f731af3e6c721a4eb32d3e8f35a09dfbd64a1939
m-tonon <math.tonon@gmail.com>
2023-03-07 06:17:31 -0300

    Revert "adding isPlatformBrowser"
    This reverts commit df092e6792173a2d7d6d328978411601b2c4a4fe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from './auth/auth.service';
@@ -15,12 +14,9 @@ export class AppComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private store: Store<fromApp.AppState>,
-    @Inject(PLATFORM_ID) private platformId) {}
+    private store: Store<fromApp.AppState>) {}
 
   ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.store.dispatch(new AuthActions.AutoLogin);
-    }
+    this.store.dispatch(new AuthActions.AutoLogin);
   }
 }
